d0cb705da2aa8e2fbbade85adce9d949bbdc1383
Wes Seago <wseago2@gmail.com>
2015-01-27 13:56:45 -0500

    Setup fragments.

---
+++ app/src/main/java/com/wesleyseago/android/ribbit/FriendsFragment.java
@@ -0,0 +1,20 @@
+package com.wesleyseago.android.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Wesley on 1/27/2015.
+ */
+public class FriendsFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/wesleyseago/android/ribbit/InboxFragment.java
@@ -0,0 +1,20 @@
+package com.wesleyseago.android.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Wesley on 1/27/2015.
+ */
+public class InboxFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+        return rootView;
+    }
+}
