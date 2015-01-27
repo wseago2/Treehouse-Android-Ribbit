1380d0f1958621a03e36ef2d45dc00b888674ca4
Wes Seago <wseago2@gmail.com>
2015-01-27 08:42:42 -0500

    Add Login Activity resources.

--- app/src/main/java/com/wesleyseago/android/ribbit/MainActivity.java
+++ app/src/main/java/com/wesleyseago/android/ribbit/MainActivity.java
@@ -1,22 +1,21 @@
 package com.wesleyseago.android.ribbit;
 
-import java.util.Locale;
-
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
@@ -40,6 +39,10 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Make app load login screen at startup
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
 
         // Set up the action bar.
         final ActionBar actionBar = getSupportActionBar();
